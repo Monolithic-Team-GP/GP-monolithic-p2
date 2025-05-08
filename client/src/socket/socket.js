206be8566a4d92ee46dc518387bd3d242b8d2c46
fuadmuchtar <tech.fuad99@gmail.com>
2025-05-08 09:51:36 +0700

    deploy-client 1

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const socket = io(import.meta.env.PROD || "https://coba-gp.azriltdkso.fun");
+const socket = io("https://coba-gp.azriltdkso.fun");
 
 export default socket;
